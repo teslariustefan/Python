3743ee6c99b6553a0983d20c59440b1b30011cf3
teslariustefan <77177679+teslariustefan@users.noreply.github.com>
2022-12-22 17:29:13 +0200

    Update
    Am adaugat link la fieacare masina.
    Am facut o comparatie bazata pe link intre masinile deja aflate in json si masina gasita la ultima rulare astfel incat sa nu avem dubluri.

--- Python C17/crawler.py
+++ Python C17/crawler.py
@@ -9,37 +9,48 @@ def main():
     soup = BeautifulSoup(page.content, "html.parser")
 
     cars = []
+    car_link = []
+    links = soup.find_all("a",{"class": ["ooa-dmsc5z eomulv92", "ooa-1rfdfx4 eomulv92"]})
 
-    for section in soup.find_all("section",{"class": "ooa-af2m8r e2nxjc60"}):
+    #am luat toate link-urile masinilor de pe prima pagina
+    for i in links:
+        car_link.append(str(i["href"]))
 
-        car_title = section.find("h2", {"class": "e1lmj3dz0 ooa-16u688i er34gjf0"}).text
-
-        car_price = section.find("div", {"class": "ooa-80vtuv er34gjf0"}).span.text
-
-        #used to get year and km
-        car_caract = section.find("ul", {"class": "ooa-zzhv62 e16henwp0"})
-
-        car_year = car_caract.find_all("li")[0].text.replace(" ", "")
-
-        car_km = car_caract.find_all("li")[1].text
-
-        car = {
-            "title": car_title,
-            "price": car_price,
-            "year": car_year,
-            "km": car_km,
-        }
-        cars.append(car)
-
+    i = 0
+    #deschidem fisierul ca sa putem compara masinile deja in fisier cu masina noastra gasita acum pe pagina
     with open("cars.json", "r+") as f:
-        # get cars from file
         try:
-            jsoncars = json.load(f)
+            cars = json.load(f)
         except:
-            jsoncars = []
-        jsoncars.append(cars)
-        f.seek(0)
-        json.dump(jsoncars, f, indent=4)
+            cars = []
+        for section in soup.find_all("section",{"class": "ooa-af2m8r e2nxjc60"}):
+                car_title = section.find("h2", {"class": "e1lmj3dz0 ooa-16u688i er34gjf0"}).text
+
+                car_price = section.find("div", {"class": "ooa-80vtuv er34gjf0"}).span.text
+
+                #used to get year and km
+                car_caract = section.find("ul", {"class": "ooa-zzhv62 e16henwp0"})
+
+                car_year = car_caract.find_all("li")[0].text.replace(" ", "")
+
+                car_km = car_caract.find_all("li")[1].text
+
+                car = {
+                    "link": car_link[i],
+                    "title": car_title,
+                    "price": car_price,
+                    "year": car_year,
+                    "km": car_km,
+                }
+                i += 1
+                #daca nu am gasit masina o adaugam in fisier
+                try:
+                    if car["link"] != cars["link"]:
+                        cars.append(car)
+                except:
+                    cars.append(car)
+                f.seek(0)
+                json.dump(cars, f, indent=4)
 
 main()
 
